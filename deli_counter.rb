ddf0b41c1dfbc1bf93e120ee400e55ff5a047d5a
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-05 21:09:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,11 +19,12 @@ def now_serving
   if array.length >=1 
     array 2 = []
     counter = 1 
-    array2.push("#{name}")
-    counter += 1 
-  end
-    puts "Currently serving #{name}."
-  else
-    puts "There is nobody waiting to be served!"
+    array.each do |name|
+      array2.push("#{name}")
+      counter += 1 
+    end
+      puts "Currently serving #{name}."
+    else
+      puts "There is nobody waiting to be served!"
   end
 end 
