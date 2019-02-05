b755ebe9b5bfed6a4b2a9f4087c14f694a634a7e
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-05 21:25:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,11 +15,11 @@ def line (array)
     end
   end
 def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
+  if line.length == 0 
     puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    puts "Currently serving #{line[0]}." 
+    line.shift 
   end
 end
 # def now_serving
