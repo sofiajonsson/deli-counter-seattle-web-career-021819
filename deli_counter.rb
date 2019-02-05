6e9f0ba099b4ec36be76955bf7497cb6c920e322
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-05 20:57:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,19 @@
 # Write your code here.
 katz_deli = []
 
-# def line (array)
-#   array2 = []
-#   counter = 1 
-#   array.each do |name|
-#       array2.push("#{counter}. #{name}")
-#       counter += 1 
-#     end 
-#     puts "The line is currently: #{array2.join(" ")}"
-#   else
-#     puts "The line is currently empty."
-#   end
-# end
+def line (array)
+  if array.length >= 1
+    array2 = []
+    counter = 1 
+    array.each do |name|
+        array2.push("#{counter}. #{name}")
+        counter += 1 
+      end 
+      puts "The line is currently: #{array2.join(" ")}"
+    else
+      puts "The line is currently empty."
+    end
+  end
 
 def line(array) # this was the one I figured out
   if array.length >= 1
