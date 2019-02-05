53e91e5fc26696832d7ff06064108641b8c0bd33
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-05 21:31:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,8 @@ def line (array)
 
 def take_a_number(line, new_person)
   line.push(new_person)
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
 
 def now_serving(line)
   if line.length == 0 
